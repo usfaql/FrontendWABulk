7ef338e3a6cb77aeb0bdf9d178fcbbf725830ca8
Yousef Abuaqel <146733467+usfaql@users.noreply.github.com>
2024-09-15 04:50:26 +0300

    Update CountryContribution.js

--- src/CountryContribution/CountryContribution.js
+++ src/CountryContribution/CountryContribution.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
 function CountryContribution({ countries, totalNumbers }) {
+  const [sortOrder, setSortOrder] = useState('desc');
+
+  const sortData = () => {
+    if (!countries || countries.length === 0) return [];  
+
+    const sortedCountries = [...countries].sort((a, b) => {
+      if (sortOrder === 'asc') {
+        return a.numbers - b.numbers;
+      } else {
+        return b.numbers - a.numbers; 
+      }
+    });
+    return sortedCountries;
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const sortedCountries = sortData();
+
   return (
     <table className='container-table-country-data'>
       <thead>
         <tr>
           <th style={{ border: '1px solid #3398db', padding: '8px' }}>Country</th>
-          <th style={{ border: '1px solid #3398db', padding: '8px' }}>Number of Numbers</th>
+          <th style={{ border: '1px solid #3398db', padding: '8px', cursor: 'pointer' }} onClick={toggleSortOrder}>
+            Number of Numbers
+            {sortOrder === 'asc' ? ' ↑' : ' ↓'} {/* Display sort direction */}
+          </th>
           <th style={{ border: '1px solid #3398db', padding: '8px' }}>Contribution (%)</th>
         </tr>
       </thead>
       <tbody>
-        {countries?.map((country, index) => {
+        {sortedCountries?.map((country, index) => {
           const contribution = (country.numbers / totalNumbers) * 100;
           return (
-            <tr key={index} style={{ overflowY:"auto"}}>
+            <tr key={index} style={{ overflowY: 'auto' }}>
               <td style={{ border: '1px solid #3398db', padding: '8px' }}>{country.name}</td>
               <td style={{ border: '1px solid #3398db', padding: '8px' }}>{country.numbers.toLocaleString()}</td>
               <td style={{ border: '1px solid #3398db', padding: '8px' }}>{contribution.toFixed(2)}%</td>
@@ -25,6 +49,6 @@ function CountryContribution({ countries, totalNumbers }) {
       </tbody>
     </table>
   );
-}
+};
 
 export default CountryContribution;

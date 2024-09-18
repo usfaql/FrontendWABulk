77f428f8e000cbb365864f4174ac76ad0ad389de
= <=>
2024-09-18 05:18:48 +0300

    Add Login Page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import './App.css';
 import Navbar from './Navbar/Navbar';
 import LeftControl from './LeftControl/LeftControl';
 import Home from './Home/Home';
-import { useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import Dashboard from './Dashboard/Dashboard';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
+import Login from './Login/Login';
+export const userContext = createContext();
+function PrivateRoute({ element, isLoggedIn }) {
+  return isLoggedIn ? element : <Navigate to="/login" />;
+}
 
 function App() {
-  
+
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
+  useEffect(() => {
+    setIsLoggedIn(!!token);
+  }, [token]);
 
   return (
+    <userContext.Provider value={{token, setToken, isLoggedIn , setIsLoggedIn}}>
       <div className="App">
-        <Navbar/>
+
+        {!isLoginPage && <Navbar />}
         <Routes>
-          <Route path='/' element={<Dashboard/>}/>
-          <Route path='/data' element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<PrivateRoute element={<Dashboard />} isLoggedIn={isLoggedIn} />} />
+        <Route path="/data" element={<PrivateRoute element={<Home />} isLoggedIn={isLoggedIn} />} />
         </Routes>
         
       </div>
-    
+    </userContext.Provider>
   );
 }
 

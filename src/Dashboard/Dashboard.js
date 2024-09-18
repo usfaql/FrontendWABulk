7bc9eaf2d25d727c04634c5690003ca65d1ebb1e
Yousef Abuaqel <146733467+usfaql@users.noreply.github.com>
2024-09-18 18:10:15 +0300

    Update Dashboard.js

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Goal from '../GoalComponant/Goal';
 import CountryContribution from '../CountryContribution/CountryContribution';
